a4dc174f2981d7c38dcef23b47e65739a4e6e99f
Jens Rosseel <slaythehorizon.games@gmail.com>
2022-03-20 19:21:55 +0100

    Add home page layout and css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,13 @@
 @extends('layout')
 @section('content')
-    <h1>HOME</h1>
+    <div class="home">
+        <div class="welcome">
+            Choose a planet to know more about
+        </div>
+        <div class="planets">
+            @foreach ($planets as $key => $planet)
+                <a href={{ route('planet', ['page' => $planet->name ]) }}><img src={{ asset('img/' . $planet->name . '.jpg') }} alt={{ $planet->name }}></a>
+            @endforeach
+        </div>
+    </div>
 @endsection
